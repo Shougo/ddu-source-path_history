11808e534315e31efd17fb02164efc30e5419ca8
Shougo Matsushita <Shougo.Matsu@gmail.com>
2023-08-15 10:46:35 +0900

    Fix Params

--- denops/@ddu-sources/path_history.ts
+++ denops/@ddu-sources/path_history.ts
@@ -2,11 +2,16 @@ import {
   BaseSource,
   Context,
   Item,
-} from "https://deno.land/x/ddu_vim@v2.4.0/types.ts";
-import { Denops, vars } from "https://deno.land/x/ddu_vim@v2.4.0/deps.ts";
-import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.3.2/file.ts";
+  TreePath,
+} from "https://deno.land/x/ddu_vim@v3.5.0/types.ts";
+import { Denops, pathsep, vars } from "https://deno.land/x/ddu_vim@v3.5.0/deps.ts";
+import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.5.3/file.ts";
 
-type Params = Record<never, never>;
+function convertTreePath(treePath: TreePath) {
+  return typeof treePath === "string" ? treePath : treePath.join(pathsep);
+}
+
+type Params = Record<string, never>;
 
 export class Source extends BaseSource<Params> {
   override kind = "file";
@@ -18,6 +23,7 @@ export class Source extends BaseSource<Params> {
   }): Promise<void> {
     this.uiName = await vars.b.get(args.denops, "ddu_ui_name", "");
   }
+
   override gather(args: {
     denops: Denops;
     sourceParams: Params;
@@ -27,7 +33,8 @@ export class Source extends BaseSource<Params> {
       async start(controller) {
         if (uiName != "") {
           const pathHistories = (await args.denops.call(
-            "ddu#get_context", uiName) as Context).pathHistories;
+            "ddu#get_context", uiName) as Context).pathHistories.map(
+            (path) => convertTreePath(path));
 
           controller.enqueue(pathHistories.map((h: string) => ({
             word: h,
